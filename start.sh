00a80bded2ce9fbf3f5f92697ec00828658125e7
Francisco Fuentes <ffuentes@emergya.com>
2020-02-19 09:17:43 +0100

    Local domains for sri and noclook set on the start script

--- start.sh
+++ start.sh
@@ -5,5 +5,21 @@ if [ ! -f norduni/compose.yml ]; then
     exit 1
 fi
 
+# add ni.localenv.loc to hosts file
+(printf '127.0.0.1\tsri.localenv.loc\n127.0.0.1\tni.localenv.loc\n';
+) \
+    | while read -r line; do
+    if ! grep -q "^${line}$" /etc/hosts; then
+	echo "$0: Adding line '${line}' to /etc/hosts"
+	if [ "$(whoami)" = "root" ]; then
+	    echo "${line}" >> /etc/hosts
+	else
+	    echo "${line}" | sudo tee -a /etc/hosts
+	fi
+    else
+	echo "Line '${line}' already in /etc/hosts"
+    fi
+done
+
 ./bin/docker-compose -f norduni/compose.yml rm -f --all
 ./bin/docker-compose -f norduni/compose.yml up $*
